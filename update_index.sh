4b08757abd9a5b49026b62632fb66d12a33ebc05
Sebastian Śledź <sebasledz@gmail.com>
2018-03-13 17:08:48 +0100

    Updated index.

--- update_index.sh
+++ update_index.sh
@@ -6,5 +6,5 @@ echo -n "" > "$OUT"
 
 find ./GSTypings -iname "*.d.ts" | sort | while read path
 do
-	echo "///<reference path=\"$path\"/>" >> "$OUT"
+	echo "/// <reference path=\"$path\"/>" >> "$OUT"
 done
