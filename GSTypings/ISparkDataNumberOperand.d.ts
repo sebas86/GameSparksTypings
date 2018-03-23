8bdbdb148600f86a51b1135e0b596496f324af1d
Sebastian Śledź <sebasledz@gmail.com>
2018-03-23 14:51:40 +0100

    Fixed condition operands - GameSparks doesn't allow using string for number operators or number for string operator...

--- GSTypings/ISparkDataNumberOperand.d.ts
+++ GSTypings/ISparkDataNumberOperand.d.ts
@@ -1,8 +1,8 @@
 interface ISparkDataNumberOperand{
-    between(low : JSON, high : JSON) : ISparkDataCondition
-    eq(value : JSON) : ISparkDataCondition
-    gt(value : JSON) : ISparkDataCondition
-    in(values : Number[]) : ISparkDataCondition
-    lt(value : JSON) : ISparkDataCondition
-    ne(value: JSON) : ISparkDataCondition
+    between(low : number, high : number) : ISparkDataCondition
+    eq(value : number) : ISparkDataCondition
+    ne(value: number) : ISparkDataCondition
+    gt(value : number) : ISparkDataCondition
+    in(values : number[]) : ISparkDataCondition
+    lt(value : number) : ISparkDataCondition
 }
--- GSTypings/ISparkDataStringOperand.d.ts
+++ GSTypings/ISparkDataStringOperand.d.ts
@@ -1,9 +1,9 @@
 interface ISparkDataStringOperand{
-    between(low : JSON, high : JSON) : ISparkDataCondition
-    eq(value : any) : ISparkDataCondition
-    gt(value : any) : ISparkDataCondition
-    in(values : Number[]) : ISparkDataCondition
-    lt(value : any) : ISparkDataCondition
-    ne(value : any) : ISparkDataCondition
-    startsWith(value : string) : ISparkDataCondition
+    between(string: JSON, string: JSON) : ISparkDataCondition
+    eq(string: any) : ISparkDataCondition
+    gt(string: any) : ISparkDataCondition
+    in(string: Number[]) : ISparkDataCondition
+    lt(string: any) : ISparkDataCondition
+    ne(string: any) : ISparkDataCondition
+    startsWith(string: string) : ISparkDataCondition
 }
